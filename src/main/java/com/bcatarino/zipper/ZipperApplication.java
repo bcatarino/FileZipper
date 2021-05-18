8e207d0a9091ff2fafc36cd9d22075dfb87e9399
Bruno Catarino <bcatarino@gmail.com>
2021-05-18 11:02:25 +0100

    Initial commit

---
+++ src/main/java/com/bcatarino/zipper/ZipperApplication.java
@@ -0,0 +1,11 @@
+package com.bcatarino.zipper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZipperApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ZipperApplication.class, args);
+    }
+}
